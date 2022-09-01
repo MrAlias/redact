bf4e5787168c02c21662d97cf8d045fb55e6c64b
Tyler Yahn <codingalias@gmail.com>
2022-09-01 13:21:18 -0700

    Add AttributeCensor

---
+++ attribute.go
@@ -0,0 +1,64 @@
+// Copyright 2022 Tyler Yahn (MrAlias)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package redact // import "github.com/MrAlias/redact"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+const defaultReplace = "****REDACTED****"
+
+func Attributes(keys ...string) trace.TracerProviderOption {
+	r := make(map[attribute.Key]attribute.Value, len(keys))
+	for _, k := range keys {
+		r[attribute.Key(k)] = attribute.StringValue(defaultReplace)
+	}
+	censor := AttributeCensor{Replacements: r}
+	return trace.WithSpanProcessor(censor)
+}
+
+type AttributeCensor struct {
+	// Replacements is a mapping of replacement values for a set of keys.
+	//
+	// If the replacement value of a key is invalid, that replacement will not
+	// be made by the SDK.
+	Replacements map[attribute.Key]attribute.Value
+}
+
+func (f AttributeCensor) OnStart(_ context.Context, s trace.ReadWriteSpan) {
+	// The Span SetAttributes method only overwrites attributes that already
+	// exists, it does not set the span attributes to only the values passed.
+	// Therefore, determine if there are any attributes that need to be
+	// redacted and set override only those values.
+	var redacted []attribute.KeyValue
+	for _, a := range s.Attributes() {
+		if v, ok := f.Replacements[a.Key]; ok {
+			redacted = append(redacted, attribute.KeyValue{
+				Key:   a.Key,
+				Value: v,
+			})
+		}
+	}
+	if len(redacted) > 0 {
+		s.SetAttributes(redacted...)
+	}
+}
+
+func (f AttributeCensor) OnEnd(trace.ReadOnlySpan)         {}
+func (f AttributeCensor) Shutdown(context.Context) error   { return nil }
+func (f AttributeCensor) ForceFlush(context.Context) error { return nil }
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/MrAlias/redact
+
+go 1.19
+
+require (
+	go.opentelemetry.io/otel v1.9.0
+	go.opentelemetry.io/otel/sdk v1.9.0
+)
+
+require (
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/trace v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+)
